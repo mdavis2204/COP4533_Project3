cf83ae5b7f975bd4df4bbcbcdb488da40f9ce336
willzoo <zhujwill@gmail.com>
2026-04-06 19:17:02 -0400

    add backtracking function and finally return proper output for main.py for example1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-input_file = "example_input1.in"
-output_file = "example_output1.out"
+input_file = "example1/example1.in"
+output_file = "example1/example1.out"
 
 def read_inputs():
-    with open("inputs/" + input_file, "r") as f:
+    with open("tests/" + input_file, "r") as f:
         # Read the first line to get the number of letters
         num_letters = int(f.readline())
 
@@ -25,6 +25,7 @@ def HighestValLCS(A, B, values):
 
     dp = [[None]*(lenB+1) for _ in range(lenA+1)]
 
+    # Fills in the DP Table
     def OPT(i, j):
         if i == 0 or j == 0:
             return 0
@@ -38,8 +39,25 @@ def HighestValLCS(A, B, values):
             dp[i][j] = max(OPT(i-1, j), OPT(i, j-1), values[A[i-1]] + OPT(i-1, j-1))
         return dp[i][j]
 
-    return OPT(lenA, lenB)    
+    optVal = OPT(lenA, lenB)    
+
+    # Backtrack through DP array to get optimal subsequence
+    def backtrack(i, j):
+        if i == 0 or j == 0:
+            return ''
+        if A[i-1] == B[j-1]:
+            return backtrack(i-1, j-1) + A[i-1]
+        elif OPT(i-1, j) > OPT(i, j-1):
+            return backtrack(i-1, j)
+        else:
+            return backtrack(i, j-1)
+    
+    optSubseq = backtrack(lenA, lenB)
+
+    return optVal, optSubseq
 
 if __name__ == '__main__':
     num_letters, values, stringA, stringB = read_inputs()
-    print(num_letters, values, stringA, stringB)
+    optVal, optSubseq = HighestValLCS(stringA, stringB, values)
+    print(optVal)
+    print(optSubseq)
