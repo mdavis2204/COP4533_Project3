4e2a9b3ef3f2805c0f7f79097ae7f8eaa1d44a31
willzoo <zhujwill@gmail.com>
2026-04-06 18:22:15 -0400

    implement HighestValLCS top down approach

--- main.py
+++ main.py
@@ -14,11 +14,32 @@ def read_inputs():
             values[letter] = int(value)
 
         # Read the next 2 lines to get the strings
-        string1 = f.readline().strip()
-        string2 = f.readline().strip()
+        stringA = f.readline().strip()
+        stringB = f.readline().strip()
 
-        return num_letters, values, string1, string2
+        return num_letters, values, stringA, stringB
+
+def HighestValLCS(A, B, values):
+    lenA = len(A)
+    lenB = len(B)
+
+    dp = [[None]*(lenB+1) for _ in range(lenA+1)]
+
+    def OPT(i, j):
+        if i == 0 or j == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        # Use i-1 j-1 when accessing string because they are 0 indexed 
+        # (our DP array is 1-indexed, dp[0] is a stand in for empty string)
+        if A[i-1] != B[j-1]:
+            dp[i][j] = max(OPT(i-1, j), OPT(i, j-1))
+        elif A[i-1] == B[j-1]:
+            dp[i][j] = max(OPT(i-1, j), OPT(i, j-1), values[A[i-1]] + OPT(i-1, j-1))
+        return dp[i][j]
+
+    return OPT(lenA, lenB)    
 
 if __name__ == '__main__':
-    num_letters, values, string1, string2 = read_inputs()
-    print(num_letters, values, string1, string2)
+    num_letters, values, stringA, stringB = read_inputs()
+    print(num_letters, values, stringA, stringB)
